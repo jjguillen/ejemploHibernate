75cb6e303177e3ea9189a44885a8a406b2e36880
jjguillen <jjavierguillen@gmail.com>
2026-04-20 18:07:55 +0200

    Commit inicial

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
+import entities.Recinto;
+import org.hibernate.Session;
+import utils.HibernateUtil;
 
 public class Main {
+
+    static void main() {
+
+        Session sesion = HibernateUtil.getSessionFactory().openSession();
+        sesion.beginTransaction();
+
+        Recinto r1 = new Recinto(null, "Palacio de Deportes",
+                "Madrid", 15000, true);
+        sesion.persist(r1); //Persistir en la base de datos, lo graba en bbdd
+
+
+        sesion.getTransaction().commit(); //Termina la transacción y confirma los datoszº
+        sesion.close();
+
+
+    }
+
+
 }
